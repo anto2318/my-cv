ce4f649f0de3540dd57204dbf83b8147375a679f
Antonio Nardi <antonionardi@Antonios-MacBook-Pro.local>
2020-05-06 02:17:29 +0200

    Adding cv assets

--- resolvers/index.js
+++
@@ -1,7 +0,0 @@
-import { merge } from 'lodash';
-import pizzaResolver from './pizza.resolver';
-import ingredientResolver from './ingredient.resolver';
-
-const resolvers = merge(pizzaResolver, ingredientResolver);
-
-export default resolvers;
